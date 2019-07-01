0dac1f206902b55b756591038ae37d10b2b1733a
liaorui <liaorui@vsoontech.com>
2019-07-01 15:22:49 +0800

    .d.ts init

---
+++ src/shime-global.d.ts
@@ -0,0 +1,10 @@
+// 声明全局的 window ，不然使用 window.XX 时会报错
+declare var window: Window;
+declare var document: Document;
+
+// interface THREE extends Window {}
+
+declare module 'qs';
+declare module 'element-ui';
+declare module 'crypto-js';
+declare module 'lodash';
